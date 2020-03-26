674301855ad77d1124b94ad8203abd8e75a58ac4
kolawoleabdulazeez5@gmail.com <kolawoleabdulazeez5@gmai.com>
2020-03-26 18:09:06 +0100

    add new.js

---
+++ new.js
@@ -0,0 +1,11 @@
+let name = "Kolawole Abdulazeez Timileyin";
+console.log("My name is: " + name);
+myCourses = ["HTML","CSS","JS","C#"];
+console.log("My courses are: " + myCourses);
+const data = [];
+for (let i=0; i<=200; i++) {
+  if (i%2===0) {
+    data.push(i);
+  }
+}
+console.log(data);
